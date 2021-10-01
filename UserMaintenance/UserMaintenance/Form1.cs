ee89659b4927be874f5756a396c62bc13a2753f3
zomboripeter <peter.zombori@stud.uni-corvinus.hu>
2021-10-01 17:07:37 +0200

    tetszoleges

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
+            lblLastName.Text = Resource1.LastName;
+            lblFirstName.Text = Resource1.FirstName;
+            btnAdd.Text = Resource1.Add;
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = txtLastName.Text,
+                FirstName = txtFirstName.Text
+            };
+            users.Add(u);
         }
     }
 }
