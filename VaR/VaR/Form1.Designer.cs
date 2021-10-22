ae0b7cda8337f7a303047fdeba2a7c33e83ddb50
Péter Zombori <peter.zombori@stud.uni-corvinus.hu>
2021-10-22 11:39:47 +0200

    ticks

--- VaR/VaR/Form1.Designer.cs
+++ VaR/VaR/Form1.Designer.cs
@@ -30,12 +30,108 @@ namespace VaR
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.portfolioDataSet = new VaR.PortfolioDataSet();
+            this.tickBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tickTableAdapter = new VaR.PortfolioDataSetTableAdapters.TickTableAdapter();
+            this.tickidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.indexDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tradingDayDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.volumeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portfolioDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tickBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tickidDataGridViewTextBoxColumn,
+            this.indexDataGridViewTextBoxColumn,
+            this.tradingDayDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn,
+            this.volumeDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tickBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 14);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(703, 410);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // portfolioDataSet
+            // 
+            this.portfolioDataSet.DataSetName = "PortfolioDataSet";
+            this.portfolioDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tickBindingSource
+            // 
+            this.tickBindingSource.DataMember = "Tick";
+            this.tickBindingSource.DataSource = this.portfolioDataSet;
+            // 
+            // tickTableAdapter
+            // 
+            this.tickTableAdapter.ClearBeforeFill = true;
+            // 
+            // tickidDataGridViewTextBoxColumn
+            // 
+            this.tickidDataGridViewTextBoxColumn.DataPropertyName = "Tick_id";
+            this.tickidDataGridViewTextBoxColumn.HeaderText = "Tick_id";
+            this.tickidDataGridViewTextBoxColumn.Name = "tickidDataGridViewTextBoxColumn";
+            this.tickidDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // indexDataGridViewTextBoxColumn
+            // 
+            this.indexDataGridViewTextBoxColumn.DataPropertyName = "Index";
+            this.indexDataGridViewTextBoxColumn.HeaderText = "Index";
+            this.indexDataGridViewTextBoxColumn.Name = "indexDataGridViewTextBoxColumn";
+            // 
+            // tradingDayDataGridViewTextBoxColumn
+            // 
+            this.tradingDayDataGridViewTextBoxColumn.DataPropertyName = "TradingDay";
+            this.tradingDayDataGridViewTextBoxColumn.HeaderText = "TradingDay";
+            this.tradingDayDataGridViewTextBoxColumn.Name = "tradingDayDataGridViewTextBoxColumn";
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "Price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "Price";
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            // 
+            // volumeDataGridViewTextBoxColumn
+            // 
+            this.volumeDataGridViewTextBoxColumn.DataPropertyName = "Volume";
+            this.volumeDataGridViewTextBoxColumn.HeaderText = "Volume";
+            this.volumeDataGridViewTextBoxColumn.Name = "volumeDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portfolioDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tickBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private PortfolioDataSet portfolioDataSet;
+        private System.Windows.Forms.BindingSource tickBindingSource;
+        private PortfolioDataSetTableAdapters.TickTableAdapter tickTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tickidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn indexDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tradingDayDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn volumeDataGridViewTextBoxColumn;
     }
 }
 
