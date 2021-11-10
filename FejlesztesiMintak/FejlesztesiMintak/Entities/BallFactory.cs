4383d4ca8a8cc61b28f2846c84b738e100e6908e
Péter Zombori <peter.zombori@stud.uni-corvinus.hu>
2021-11-10 10:38:51 +0100

    Ball factory

---
+++ FejlesztesiMintak/FejlesztesiMintak/Entities/BallFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FejlesztesiMintak.Entities
+{
+    public class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+
+    }
+}
